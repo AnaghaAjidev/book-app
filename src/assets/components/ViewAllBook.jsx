c1b8908d3dd209fed2bf497fa672103915c07aff
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-10 10:29:03 +0530

    Commit new changes

--- src/assets/components/ViewAllBook.jsx
+++ src/assets/components/ViewAllBook.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BookHeader from './BookHeader'
 
-const books = [
+const ViewAllBook = () => {
+const [book,changeBooks] = useState ( [
   { id: 1, title: "The Alchemist", author: "Paulo Coelho", price: "₹399", img: "https://picsum.photos/200?1" },
   { id: 2, title: "Harry Potter", author: "J. K. Rowling", price: "₹699", img: "https://picsum.photos/200?2" },
   { id: 3, title: "The Hobbit", author: "J. R. R. Tolkien", price: "₹499", img: "https://picsum.photos/200?3" },
@@ -13,14 +14,15 @@ const books = [
   { id: 9, title: "The Psychology of Money", author: "Morgan Housel", price: "₹650", img: "https://picsum.photos/200?9" },
   { id: 10, title: "Deep Work", author: "Cal Newport", price: "₹520", img: "https://picsum.photos/200?10" }
 ]
+ )
 
-const ViewAllBook = () => {
+
   return (
     <>
       <BookHeader />
       <div className="container mt-4">
         <div className="row g-4">
-          {books.map((book) => (
+          {book.map((book) => (
             <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={book.id}>
               <div className="card h-100 shadow">
                 <img
